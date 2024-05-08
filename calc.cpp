fd38c847f08aff6b0b66318988026191340d7def
IgorAralbaev <168334701+IgorAralbaev@users.noreply.github.com>
2024-05-08 14:59:55 +0500

    Add files via upload

---
+++ calc.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	char c;
+	cin >> a >> b;
+	cin >> c;
+	if (c == '+') {
+		cout << a + b;
+	} else if (c == '-'){
+		cout << a - b;
+	} else if (c == '*'){
+		cout << a * b;
+	} else if (c == '/'){
+		cout << double(a) / double(b);
+	}
+}
+
